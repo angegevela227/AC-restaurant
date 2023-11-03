b115464095352d9e812664bb4fe7188b42ead550
angegevela227 <115968783+angegevela227@users.noreply.github.com>
2023-11-03 21:20:05 +0800

    Delete converstion.py

--- converstion.py
+++
@@ -1,5 +0,0 @@
-#convertcelciustofarenheit
-celcius = float(input("enter the value of celcius in degree: "))
-#formula
-farenheit =(celcius * 1.8) + 32
-print("the value temperature in farenheit: ",farenheit)
